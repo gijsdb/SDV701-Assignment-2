0239ab697c3131f971693752197f5579321c799b
Gijs De Blauw <Gijs-DeBlauw@live.nmit.ac.nz>
2020-05-06 16:01:32 +1200

    set up localhost selfhost, working on db

--- sdv701-selfHost/Program.cs
+++ sdv701-selfHost/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
 
 namespace sdv701_selfHost
 {
@@ -10,6 +12,22 @@ namespace sdv701_selfHost
     {
         static void Main(string[] args)
         {
+            // Configure server
+            Uri _baseAddress = new Uri("http://localhost:60064/");
+            HttpSelfHostConfiguration config = new HttpSelfHostConfiguration(_baseAddress);
+            config.Routes.MapHttpRoute(
+              name: "DefaultApi",
+              routeTemplate: "api/{controller}/{action}/{id}",
+              defaults: new { id = RouteParameter.Optional }
+            );
+            // Create server
+            HttpSelfHostServer server = new HttpSelfHostServer(config);
+            // Start server
+            server.OpenAsync().Wait();
+            Console.WriteLine("CameraCo server is live on " + _baseAddress);
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+            server.CloseAsync().Wait();
         }
     }
 }
