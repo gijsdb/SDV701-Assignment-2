a3e800dfeaee23a6e701e69600fbe9793ba4bf88
Gijs De Blauw <Gijs-DeBlauw@live.nmit.ac.nz>
2020-05-27 11:25:12 +1200

    Retrieves brand names from API

--- sdv701-customer-app/pgBrands.xaml.cs
+++ sdv701-customer-app/pgBrands.xaml.cs
@@ -7,11 +7,6 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -20,11 +15,23 @@ namespace sdv701_customer_app
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class pgBrands : Page
     {
-        public MainPage()
+        public pgBrands()
         {
             this.InitializeComponent();
+        }
+
+        private async void pgBrands_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                lstBrands.ItemsSource = await ServiceClient.GetBrandNamesAsync();
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = ex.GetBaseException().Message;
+            }
         }
     }
 }
