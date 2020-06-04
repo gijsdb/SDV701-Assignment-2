5e641bb9ae56368db29f873485f0202b17c9f1d5
Gijs De Blauw <Gijs-DeBlauw@live.nmit.ac.nz>
2020-06-05 10:31:48 +1200

    Removed unused usings

--- sdv701-customer-app/pgOrder.xaml.cs
+++ sdv701-customer-app/pgOrder.xaml.cs
@@ -1,22 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Popups;
 using Windows.UI.Xaml.Navigation;
 using static sdv701_customer_app.clsDTO;
-using System.Text.RegularExpressions;
-using System.Globalization;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
