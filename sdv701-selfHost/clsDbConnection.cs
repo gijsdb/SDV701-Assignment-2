7696c4519bfdeecb4d570a85618651ac2cb3e216
Gijs De Blauw <Gijs-DeBlauw@live.nmit.ac.nz>
2020-06-08 11:11:24 +1200

    removed unused usings

--- sdv701-selfHost/clsDbConnection.cs
+++ sdv701-selfHost/clsDbConnection.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.SqlClient;
 
 namespace sdv701_selfHost
